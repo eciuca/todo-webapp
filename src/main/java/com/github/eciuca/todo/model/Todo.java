dd1bcccd7dc290d03453569e9af211b50e0bd58d
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-02-28 22:36:10 +0200

    Create Todo model

---
+++ src/main/java/com/github/eciuca/todo/model/Todo.java
@@ -0,0 +1,23 @@
+package com.github.eciuca.todo.model;
+
+public class Todo {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
